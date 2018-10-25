950b87206a3dae39df9e5e20685f9a81b93588e6
Scott Ballantyne <ussballantyne@gmail.com>
2018-10-25 10:08:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+mimi-serve (把秘密放在云)
+=========
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ballantyne/mimi-serve)
+
+To put your encrypted secrets in the cloud, click the deploy button above and then clone the repo and add these configuration settings.  This repo should be very easy to modify so that it can be used with other cloud providers.  If someone wants to do that work I welcome the contributions.
+
+```bash
+  heroku config:set AWS_ACCESS_KEY_ID=key
+  heroku config:set AWS_SECRET_ACCESS_KEY=secret
+  heroku config:set AWS_REGION=region
+  heroku config:set AWS_BUCKET=bucket
+```
+
+Contributing
+------------
+
+If you'd like to contribute a feature or bugfix: Thanks! To make sure your fix/feature has a high chance of being included, please read the following guidelines:
+
+1. Post a [pull request](https://github.com/ballantyne/mimi-serve/compare/).
+2. Make sure there are tests! We will not accept any patch that is not tested.
+   It's a rare time when explicit tests aren't needed. If you have questions
+   about writing tests for paperclip, please open a
+   [GitHub issue](https://github.com/ballantyne/mimi-serve/issues/new).
+
+
+And once there are some contributors, then I would like to thank all of [the contributors](https://github.com/ballantyne/mimi-serve/graphs/contributors)!
+
+
+Donations
+------------
+
+If you'd like to contribute with bitcoin or another cryptocurrency you can send coins to the addresses below:
+
+* ETH: 0xc3Cc87CFD19521e55c27832EdDb2cAFE2577F28E
+* BTC: 1CqyYz717jUwENBraXAVr8hZtnK8k23vPK
+* BCH: 129mMPtwjKce54FGE6rsRE4Ty2wFCKeQmr
+* LTC: LPvwrQjYzTfE8DJFmpdcpjNw9zeuhxhdE6
+
+License
+-------
+
+It is free software, and may be redistributed under the terms specified in the MIT-LICENSE file.
+
+Copyright
+-------
+© 2018 Scott Ballantyne. See LICENSE for details.
--- index.js
+++ index.js
@@ -2,35 +2,69 @@ var path = require('path');
 var http = require('http');
 var AWS = require('aws-sdk');
 
+var verbose = true;
+
 AWS.config = new AWS.Config({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION
 });
 
-var modifyUrl = function(url) {
-  var urlComponents = url.split('/')
+var modifyUrl = function(host, url) {
+  var urlComponents = url.slice(1, url.length).split('/')
   var filename = [new Date().getTime().toString(), urlComponents.pop()].join('-');
   urlComponents.push(filename);
+  if (process.env.INCLUDE_HOST) {
+    urlComponents.unshift(host);
+  }
   url = urlComponents.join('/');
-  return url.slice(1, url.length);
+  return url;
+}
+
+var parameterizeHost = function(host) {
+  return host.split('.').join('-').split(':').join("-").replace('mimi-', '');
+}
+
+var setVerbose = function(req) {
+  if (req.url == '/verbose') {
+    if (verbose) {
+      verbose = false;
+    } else {
+      verbose = true;
+    }
+  }
 }
 
 var server = http.createServer(function (req, res) {
-  var s3obj = new AWS.S3({
-    params: {
-      Bucket: process.env.AWS_BUCKET,
-      Key: modifyUrl(req.url)
-    }
-  });
-
-  s3obj.upload({Body: req})
-    .on('httpUploadProgress', function(evt) { console.log(evt); })
-    .send(function(err, data) { console.log(err, data) });
-
-  req.on('end', function() {
-    res.write('{"success": true}');
+  if (req.method == 'GET') {
+    setVerbose(req);
+    res.write('<html><head><title>秘密</title><meta charset="UTF-8"></head><body>秘密</body></html>');
     res.end();
-  })
+  }
+  if (req.method == 'POST') {
+    var s3obj = new AWS.S3({
+      params: {
+        Bucket: process.env.AWS_BUCKET,
+        Key: modifyUrl(parameterizeHost(req.headers.host), req.url)
+      }
+    });
+
+    s3obj.upload({Body: req})
+      .on('httpUploadProgress', function(evt) { 
+        if (verbose) {
+          console.log(evt); 
+        }
+      })
+      .send(function(err, data) { 
+        if (verbose) {
+          console.log(err, data) 
+        }
+      });
+
+    req.on('end', function() {
+      res.write('{"success": true}');
+      res.end();
+    })
+  }
 });
 server.listen(process.env.PORT, '0.0.0.0');
